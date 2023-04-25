ebc09d22bea6e6d5ba8eba5ac4c437edfeb89447
keynix <keinyagihashi@gmail.com>
2023-04-25 11:41:06 +0900

    初期

---
+++ Assets/keine/CPData.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CPData : MonoBehaviour
+{
+    public static bool Right = true;
+    
+
+    public static int ArmShot_norml_Lv = 1;
+    public static bool ArmShot_norml_Lv1 = true;
+    public static bool ArmShot_norml_Lv2 = false;
+
+    public static int ArmShot_Penetration_Lv = 1;
+    public static bool ArmShot_Penetration_Lv1 = true;
+    public static float Penetration_speed=500.0f;
+
+
+
+    public static bool CustomNo1 = true;
+    public static bool CustomNo2 = false;
+
+
+    public static bool CustomHigh = false;
+
+    public static bool BOSS_start = false;
+
+
+    //ボスのスクリプトにて使用
+    public static bool LastResort = false;
+
+}
